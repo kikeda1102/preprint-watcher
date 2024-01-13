56bc876842900b4f1ded5b47a794632711aca99d
kikeda1102 <kikeda1102@gmail.com>
2024-01-13 14:58:09 +0900

    initial commit

---
+++ app/overview/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+
+export default function Page() {
+    return <Link href='/'>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Back to Home </button>
+    </Link>;
+}
