21219c9ecd5c474ce8e7aa40359f0c0c9c119fde
kikeda1102 <kikeda1102@gmail.com>
2024-01-14 20:00:15 +0900

    kireinishita

--- app/overview/[id]/page.tsx
+++ app/overview/[id]/page.tsx
@@ -1,32 +1,17 @@
-// page.tsx
 import Link from 'next/link';
-import { parseArxivData } from '@/lib/actions';
+import { parseData } from '@/lib/actions';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
 
-    // データの取得とパース
-    const fetchData = async () => {
-        try {
-            const arxivData = await parseArxivData();
-            const entries = arxivData?.feed?.entry || [];
-            return entries;
-        } catch (error) {
-            console.error('Error fetching or parsing data:', error);
-            // エラーが発生した場合の処理をここに追加
-            return [];
-        }
-    };
-
     const displayEntries = async () => {
-        const entries = await fetchData();
+        const entries = await parseData();
 
         // 検索結果を表示
         return entries.map((entry: any) => (
             <div key={entry.id}>
                 <h3>{entry.title}</h3>
                 <p>{entry.summary}</p>
-                {/* 他の情報も必要に応じて表示 */}
             </div>
         ));
     };
@@ -36,7 +21,6 @@ export default async function Page({ params }: { params: { id: string } }) {
             <h1>Overview</h1>
             <p>Viewing user {id}</p>
 
-            {/* 検索結果を表示 */}
             {await displayEntries()}
 
             <p> TODO: 登録済みのキーワード一覧を表示 </p>
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,17 +1,23 @@
 'use server';
 import { XMLParser } from 'fast-xml-parser';
 
-export async function fetchArxivData() {
+async function fetchData() {
     const url = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=2';
     const response = await fetch(url);
     const data = await response.status === 200 ? response.text() : 'error fetching data';
-    // console.log(data);
     return data;
 }
 
-export async function parseArxivData() {
-    const data = await fetchArxivData();
-    const parser = new XMLParser();
-    const jsonData = parser.parse(data);
-    return jsonData;
+export async function parseData() {
+    try {
+        const data = await fetchData();
+        const parser = new XMLParser();
+        const jsonData = parser.parse(data);
+        const entries = jsonData.feed.entry || [];
+        return entries;
+
+    } catch (error) {
+        console.error('Error fetching or parsing data:', error);
+        return [];
+    }
 }
