f95175754b74ef5f91870742dee140278e181139
kikeda1102 <kikeda1102@gmail.com>
2024-01-15 17:28:53 +0900

    体裁ととのえ

--- components/overview/searchResult.tsx
+++ components/overview/searchResult.tsx
@@ -4,21 +4,43 @@ export default async function searchResult() {
 
     const entries = await parseData();
 
+    function formatDate(dateString: string): string {
+        const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
+        return formattedDate;
+    }
+
     // 検索結果を表示
     return (
-        entries.map((entry: any) => (
-            console.log(entry.author),
-            <div key={entry.id} >
-                <h1 className="text-2xl font-bold">{entry.title} </h1>
-                <p className="text-sm"> {entry.published} </p>
-                {entry.author.map((author: any) => (
-                    <span key={author.name} className="text-xl"> {author.name}, </span>
-                ))
-
-                }
-
-                <p> {entry.summary} </p>
+        <>
+            <div>
+                found {entries.length} papers.
             </div>
-        ))
+
+            {entries.map((entry: any) => (
+                // console.log(entry.author),
+                <div key={entry.id} className='m-10'>
+                    <a href={`${entry.id}`} target="_blank" className="text-blue-500 hover:underline" rel="noopener noreferrer">
+                        {entry.id}
+                    </a>
+                    <h1 className="text-2xl font-bold mb-2">{entry.title}</h1>
+                    <p className="text-sm mb-2">{formatDate(entry.published)}</p>
+                    {entry.author.map((person: any, index: number) => (
+                        <span key={person.name} className="text-xl">
+                            {person.name}
+                            {index < entry.author.length - 1 && ', '}
+                        </span>
+                    ))}
+                    <div>
+
+                        {entry.summary.length > 200
+                            ? entry.summary.slice(0, 500) + '...'
+                            : entry.summary
+                        }
+
+                    </div>
+                </div>
+            ))}
+        </>
     );
 };
