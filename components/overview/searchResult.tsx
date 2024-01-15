4441ca222f8fc82798bf66c3844dd945d9ed8fb6
kikeda1102 <kikeda1102@gmail.com>
2024-01-15 12:03:36 +0900

    体裁ととのえ

--- components/overview/searchResult.tsx
+++ components/overview/searchResult.tsx
@@ -7,13 +7,18 @@ export default async function searchResult() {
     // 検索結果を表示
     return (
         entries.map((entry: any) => (
-            <>
-                <div key={entry.id} >
-                    <h3>{entry.title} </h3>
-                    < p > {entry.summary} </p>
-                </div>
+            console.log(entry.author),
+            <div key={entry.id} >
+                <h1 className="text-2xl font-bold">{entry.title} </h1>
+                <p className="text-sm"> {entry.published} </p>
+                {entry.author.map((author: any) => (
+                    <span key={author.name} className="text-xl"> {author.name}, </span>
+                ))
 
-            </>
+                }
+
+                <p> {entry.summary} </p>
+            </div>
         ))
     );
 };
